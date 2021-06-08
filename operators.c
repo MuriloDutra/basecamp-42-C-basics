8a307f718923f51d69349c76cdb59949bb7f9c92
mdutra-a <mdutra-a@student.42sp.org.br>
2021-06-08 21:57:32 +0000

    Operators

---
+++ operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	int value = 2;
+
+	i = 42 + 8;
+	i = 42 - 8;
+	i = 42 / 8;
+	i = 42 * 8;
+	i = 42 % 8; //The rest of the division
+	i = 42 * value;
+	printf("%d\n", i);
+}
